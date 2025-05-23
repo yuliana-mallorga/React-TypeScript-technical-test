e79bf04c35334e9b9e5e5fd0dd64127bcec5cc04
Yuliana Mallorga <yulianamallorga@gmail.com>
2025-05-23 12:23:05 -0300

    Get and save users from the randomuser API

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("https://randomuser.me/api?results=100")
+        console.log("respuesta de usuarios",res.ok);
+        
+        if (!res.ok) throw new Error(`HTTPS error! status: ${res.status}`)
+      
+        const data = await res.json()
+        setUsers(data.results)
+        } catch (err) {
+         console.error(`Error getting users: ${err}`)
+      }
+    }
 
+    fetchUsers()
+    
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <h1>Test</h1>
+        { JSON.stringify(users)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
