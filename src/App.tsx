4fa90635a39e0d578d605fe12324892ce1b734dd
Yuliana Mallorga <yulianamallorga@gmail.com>
2025-05-30 14:06:51 -0300

    Add menu to toggle colors and sort users by country.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,26 @@ import  UserList from './component/UsersList'
 
 function App() {
   const [users, setUsers] = useState<User[]>([])
+  const [showColors, setShowColors] = useState(false)
+  const [sortCountry, setSortCountry] = useState(false)
+  const [ sortUserbyCountry, setSortUserbyCountry] = useState(users)
+
+  const toggleColors = () => {
+    setShowColors(!showColors)
+  }
+
+  const sortCountries = () => {
+    const nextSortState = !sortCountry
+    if(nextSortState){
+      const copyUsers = [...users]
+      const sortedUsers = copyUsers.sort((user1, user2) => user1.location.country.localeCompare(user2.location.country));
+      setSortUserbyCountry(sortedUsers)
+    } else{
+      setSortUserbyCountry(users)
+    }
+    setSortCountry(nextSortState)
+  }
+
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -13,6 +33,7 @@ function App() {
       
         const data = await res.json()
         setUsers(data.results)
+        setSortUserbyCountry(data.results)
         } catch (err) {
          console.error(`Error getting users: ${err}`)
       }
@@ -24,7 +45,28 @@ function App() {
   return (
       <div className='App'>
         <h1>Test</h1>
-        <UserList users={users}/>
+        <header>
+          <ul className="menu">
+            <li> 
+              <button type="button" onClick={toggleColors}> 
+              Change color of row
+            </button>
+            </li>
+            <li> 
+              <button type="button" onClick={sortCountries}> 
+              Sort by country
+            </button>
+            </li>
+            <li> 
+              <button type="button" onClick={toggleColors}> 
+              tests
+            </button>
+            </li>
+          </ul>
+        </header>
+        <main>
+          <UserList users={sortUserbyCountry} showColors={showColors}/>
+        </main>
       </div>
   )
 }
--- src/component/UsersList.tsx
+++ src/component/UsersList.tsx
@@ -1,47 +1,58 @@
-import { type User } from '../types.d'
+import { type User } from "../types.d";
 
 interface Props {
-  users: User[]
+  users: User[];
+  showColors: boolean;
 }
-export function UserList ({ users }: Props) {
-    return (
-        <table className="usersTable">
-          <thead>
-            <tr>
-              <th>Number</th>
-              <th>Photo</th>
-              <th>Name</th>
-              <th>Lastname</th>
-              <th>Country</th>
-              <th>Actions</th>   
+
+export function UserList({ showColors, users }: Props) {
+
+  return (
+    <table className="usersTable">
+      <thead>
+        <tr>
+          <th>Number</th>
+          <th>Photo</th>
+          <th>Name</th>
+          <th>Lastname</th>
+          <th>Country</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, index) => {
+          console.log("user", user);
+          
+          const backgroundColor = index % 2 === 0 ? '#333' : '#555';
+          const backgroundColor2 = index % 2 === 0 ? '#fff' : '#00ffcc';
+          const color = showColors ? backgroundColor : backgroundColor2;
+          
+          return (
+            <tr key={user.login.uuid} style={{backgroundColor: color}}>
+              <td>
+                { typeof index === 'number' ? index + 1 : ''}
+              </td>
+              <td>
+                <img src={user.picture.medium} alt={user.name.first} />
+              </td>
+              <td>{user.name.first}</td>
+              <td>{user.name.last}</td>
+              <td>{user.location.country}</td>
+              <td>
+                <button
+                  onClick={(user) => {
+                    console.log("onClick function is true", user.altKey, index);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            { 
-              users.map((user, index)=>{
-                return (
-                  <tr key={index}>
-                    <td>{index}</td>
-                    <td>
-                      <img src={user.picture.medium} alt={user.name.first} />
-                    </td>
-                    <td>{user.name.first}</td>
-                    <td>{user.name.last}</td>
-                    <td>{user.location.country}</td>
-                    <td> 
-                      <button onClick={(user)=>{ 
-                        console.log("onClick function is true", user.altKey, index);
-                      }}>
-                      Do Click
-                      </button>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-    )
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default UserList;
